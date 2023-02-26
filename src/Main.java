4b1c5bc1577ee8c6e53b96ae02a14486a487bc03
Mauricio <mauriciorodriguez90@aragon.unam.mx>
2023-02-26 15:45:01 -0600

    codigo de la tarea

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,10 @@ public class Main {
         System.out.print("Introduce una cadena de caracteres: ");
         String inputString = scanner.nextLine();
         boolean isConsecutive = true;
-
-        // check if the string is empty or has only one character
         if (inputString.length() == 0 || inputString.length() == 1) {
             isConsecutive = false;
         } else {
-            // convert the string to a char away
             char[] charArray = inputString.toCharArray();
-
-            //check if each character is consecutive with the previous one
             for (int i = 1; i < charArray.length; i ++) {
                 if ((int)charArray[i] - (int)charArray[i-1] !=1){
                     isConsecutive = false;
@@ -21,8 +16,6 @@ public class Main {
                 }
             }
         }
-
-        // print the result
         if (isConsecutive) {
             System.out.println("True");
         } else {
